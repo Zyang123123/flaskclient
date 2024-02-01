d478607e856a527fbe0bb81046e4381aa480f2ce
jzy <jiaoziyang0123@163.com>
2024-02-01 12:36:51 +0000

    change front end

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 BOOKS_API_URL = os.getenv('BOOKS_API_URL', 'http://bookss-api-server.f6erhqa8aqc7anbs.uksouth.azurecontainer.io:5000/books')
 
+@app.route('/', methods=['GET'])
+def index():
+    # Render an HTML form for inputting search criteria
+    return render_template('search_form.html')
+
 @app.route('/books', methods=['GET'])
 def get_books():
+    # Extract search criteria from query parameters
+    # genre = request.args.get('genre', '')
     genre = request.args.get('genre', default=None, type=str)
     '''
     genre = request.args.get('genre', type=str)
@@ -24,6 +31,7 @@ def get_books():
     # Replace this URL with the actual URL of your first service
     # response = requests.get(BOOKS_API_URL, params=query_params)
     response = requests.get(BOOKS_API_URL)
+    # response = requests.get(f"{BOOKS_API_URL}?genre={genre}")
     if response.ok:
         '''
         return jsonify(response.json())
@@ -35,10 +43,11 @@ def get_books():
             filtered_books = [book for book in books if book['genre'].lower() == genre.lower()]
         else:
             # If no genre is specified, return the third book
-            filtered_books = books[2] if len(books) >= 3 else None
+            filtered_books = books
 
-        return jsonify(filtered_books)
+        # return jsonify(filtered_books)
         
+        return render_template('search_result.html', books=filtered_books)
 
     else:
         return jsonify({"error": "Bad response from the first service"}), 500
